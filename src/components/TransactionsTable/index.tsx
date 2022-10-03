f65dd2b59cf71af3011d64bc06119cbef80e4c53
murillowelsi <murillo.welsi@gmail.com>
2022-10-03 17:46:37 +0100

    Adding dynamic values to mirageJS

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,23 @@
-import { useEffect } from "react";
-import { api } from "../../services/api";
-import { Container } from "./styles";
+import { useEffect, useState } from 'react'
+import { api } from '../../services/api'
+import { Container } from './styles'
+
+interface Transaction {
+  id: number
+  title: string
+  amount: number
+  type: string
+  category: string
+  createdAt: string
+}
 
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
   useEffect(() => {
-    api.get('transactions')
-      .then(response => console.log(response.data))
+    api
+      .get('transactions')
+      .then((response) => setTransactions(response.data.transactions))
   }, [])
 
   return (
@@ -21,20 +33,25 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Dev web</td>
-            <td className="deposit">€12000</td>
-            <td>Web Dev</td>
-            <td>20/02/2022</td>
-          </tr>
-          <tr>
-            <td>Rental</td>
-            <td className="withdraw">- €750</td>
-            <td>Home</td>
-            <td>01/03/2022</td>
-          </tr>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-PT', {
+                  style: 'currency',
+                  currency: 'EUR',
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-PT').format(
+                  new Date(transaction.createdAt),
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
-  );
+  )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createServer } from 'miragejs';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createServer, Model } from 'miragejs'
+import App from './App'
 
 createServer({
-  routes() {
-    this.namespace = 'api';
-    
-    this.get('/transactions', () => {
-      return [
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
         {
           id: 1,
-          title: 'Transaction 1',
-          amount: 400,
+          title: 'Website freelance',
           type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+          category: 'Fodase',
+          amount: 6000,
+          createdAt: new Date('2022-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Rental',
+          type: 'withdraw',
+          category: 'Expenses',
+          amount: 750,
+          createdAt: new Date('2022-02-16 09:00:00'),
+        },
+      ],
     })
-  }
+  },
+
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  },
 })
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
